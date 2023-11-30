3a3f6cf6df4a78265b640ffeb361e50861686707
OoxyGenV1 <149638563+OoxyGenV1@users.noreply.github.com>
2023-11-30 19:50:43 -0400

    pagina

---
+++ script.js
@@ -0,0 +1,18 @@
+function ocultarboton(){
+document.getElementById('obj1').style.display = 'none';
+}
+function mostrarboton(){
+document.getElementById('obj2').style.display = 'block';
+}
+function inciarsesion()
+{
+alert('La sesion se inicio satisfactoriamente');
+}
+function correo()
+{
+alert('Se envio un correo para restablecer la contraseña');
+}
+function crearcuenta()
+{
+alert('la cuenta se creo satisfactoriamente');
+}
